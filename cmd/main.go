5646e8f5ab091e1ed3f491c423fbefaf842b98ef
SaeedM <mzahry36@gmail.com>
2025-10-03 15:21:51 +0330

    feat: initial connection to mongodb

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "github.com/SaeedMPro/article-tag-extractor/internal/config"
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/SaeedMPro/article-tag-extractor/internal/config"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func main() {
-	config.LoadConfig()
+	cfg := config.LoadConfig()
+	log.Printf("config loaded: %v", cfg)
 
+	mongoClient, err := mongo.NewClient(cfg.Database.URL)
+	if err != nil {
+		log.Fatalf("failed to connect to MongoDB: %v", err)
+	}
+
+	// TODO: grpc server connection and graceful shutdown
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := mongoClient.Disconnect(ctx); err != nil {
+		log.Printf("Mongo disconnect error: %v", err)
+	}
+
+	log.Println("Server stopped gracefully")
 }
