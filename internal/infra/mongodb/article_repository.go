1295754f0dc8d646d9e9d978efc29dfcdad3c1bc
SaeedM <mzahry36@gmail.com>
2025-10-03 23:05:17 +0330

    refactor: improve NewArticleRepository function by enhancing index creation and error logging

--- internal/infra/mongodb/article_repository.go
+++ internal/infra/mongodb/article_repository.go
@@ -2,8 +2,11 @@ package mongodb
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/SaeedMPro/article-tag-extractor/internal/domain/entity"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,13 +14,25 @@ type ArticleRepository struct {
 	collection *mongo.Collection
 }
 
-func NewArticleRepository(client *mongo.Client, dbName string, collection string) *ArticleRepository {
+func NewArticleRepository(client *mongo.Client, dbName, collectionName string) *ArticleRepository {
+	db := client.Database(dbName)
+	coll := db.Collection(collectionName)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_, err := coll.Indexes().CreateOne(ctx, mongo.IndexModel{
+		Keys: bson.D{{Key: "tags", Value: 1}},
+	})
+	if err != nil {
+		log.Printf("failed to create index: %v\n", err)
+	}
+
 	return &ArticleRepository{
-		collection: client.Database(dbName).Collection(collection),
+		collection: coll,
 	}
 }
 
-func (r *ArticleRepository) SaveArticle(ctx context.Context, a *entity.Article) error {
+func (r *ArticleRepository) SaveArticle(ctx context.Context, article *entity.Article) error {
 	//TODO
 	return nil
 }
