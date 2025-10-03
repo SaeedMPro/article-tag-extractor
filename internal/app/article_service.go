26f03f541e00ce2373944984ac1a621e9fe0d5b9
SaeedM <mzahry36@gmail.com>
2025-10-04 01:59:18 +0330

    feat: enhance ArticleService to extract tags concurrently and save articles

--- internal/app/article_service.go
+++ internal/app/article_service.go
@@ -2,23 +2,54 @@ package app
 
 import (
 	"context"
+	"sync"
+	"time"
 
 	"github.com/SaeedMPro/article-tag-extractor/internal/domain/entity"
 	"github.com/SaeedMPro/article-tag-extractor/internal/domain/port"
 )
 
 type ArticleService struct {
-	repo port.ArticleRepository
+	repo         port.ArticleRepository
+	tagExtractor port.TagExtractor
 }
 
 func NewArticleService(repo port.ArticleRepository) *ArticleService {
-	return &ArticleService{repo: repo}
+	return &ArticleService{
+		repo:         repo,
+		tagExtractor: NewTagExtractorService(),
+	}
 }
 
 func (s *ArticleService) ProcessArticles(ctx context.Context, articles []*entity.Article) (int, error) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
 	count := 0
 
-	//TODO: implement processing article
+	// extract tags concurrently and save articles with their tags
+	for _, article := range articles {
+		wg.Add(1)
+		go func(a *entity.Article) {
+			defer wg.Done()
+			
+			tags := s.tagExtractor.ExtractTags(a.Title, a.Body)
+			a.Tags = tags
+
+			article := &entity.Article{
+				Title:     a.Title,
+				Body:      a.Body,
+				Tags:      tags,
+				CreatedAt: time.Now(),
+			}
+
+			if err := s.repo.SaveArticle(ctx, article); err == nil {
+				mu.Lock()
+				count++
+				mu.Unlock()
+			}
+		}(article)
+	}
+	wg.Wait()
 
 	return count, nil
 }
